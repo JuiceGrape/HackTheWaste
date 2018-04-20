c8a29b3835af83a8356acc03df1e3f130c3401f5
Bas Mentjot <bmentjot@gmail.com>
2018-04-20 21:38:20 +0200

    Added fire projectile

--- Hack The Waste/Assets/Scripts/Projectile.cs
+++ Hack The Waste/Assets/Scripts/Projectile.cs
@@ -3,46 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (Rigidbody2D))]
+[RequireComponent(typeof (SpriteRenderer))]
 public class Projectile : MonoBehaviour 
 {
 	private Vector2 movement;
 
-	private Player player;
+	protected Player player;
+	protected Environment environment;
 
 	[SerializeField] private float speed = 1.0f; 
 
-	[SerializeField] private Rigidbody2D rigidbody2D;
+	private Rigidbody2D rigidbody2D;
+	private SpriteRenderer renderer;
 
 	void Start()
 	{
 		rigidbody2D = GetComponent<Rigidbody2D>();
 		rigidbody2D.gravityScale = 0;
 
+		renderer = GetComponent<SpriteRenderer>();
 	}
 
-	public void create(/*environment*/ Player player, Vector2 movement)
+	public Sprite GetSprite()
 	{
+		return renderer.sprite;
+	}
+
+	public void create(Environment environment, Player player, Vector2 movement)
+	{
+		this.player = player;
 		setMovement(movement);
+		this.environment = environment;
 	}
 
 	private void setMovement(Vector2 movement)
 	{
 		this.movement = movement.normalized * speed;
-		if (movement != null)
 		rigidbody2D.velocity = this.movement;
 	}
 
-	void OnCollisionEnter2D(Collision2D collision2D)
+	void OnTriggerEnter2D(Collider2D collider2D)
     {
-		Hit();
+		Enemy enemy = collider2D.gameObject.GetComponent<Enemy>();
+		if (enemy != null)
+		{
+			Hit(enemy);
+		}
     }
 
 
 
 
-	private void Hit()
+	protected virtual void Hit(Enemy enemy)
 	{
-		 
+		 //enemy.DestroyEnemy();
+		 Destroy(this.gameObject);
 	}
 
 
