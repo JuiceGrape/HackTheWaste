632b07ddfc66a6acbd671159a7e3ec20185a40df
Bas Mentjot <bmentjot@gmail.com>
2018-04-20 21:00:33 +0200

    Added projectile script

---
+++ Hack The Waste/Assets/Scripts/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class Projectile : MonoBehaviour 
+{
+	private Vector2 movement;
+
+	private Player player;
+
+	[SerializeField] private float speed = 1.0f; 
+
+	[SerializeField] private Rigidbody2D rigidbody2D;
+
+	void Start()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D.gravityScale = 0;
+
+	}
+
+	public void create(/*environment*/ Player player, Vector2 movement)
+	{
+		setMovement(movement);
+	}
+
+	private void setMovement(Vector2 movement)
+	{
+		this.movement = movement.normalized * speed;
+		if (movement != null)
+		rigidbody2D.velocity = this.movement;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D)
+    {
+		Hit();
+    }
+
+
+
+
+	private void Hit()
+	{
+		 
+	}
+
+
+
+}
