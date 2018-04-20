d7778066cdde611b9fbde7fcca21376f1db779d1
xXsjorsdudeXx <sjors.iven@gmail.com>
2018-04-20 20:41:13 +0200

    Bugfix

--- Hack The Waste/Assets/Scripts/PlayerTurret.cs
+++ Hack The Waste/Assets/Scripts/PlayerTurret.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerTurret : MonoBehaviour
 {
 	private Camera mainCamera;
+	public Transform turretChildTransform;
 	public bool usingController;
 	// Use this for initialization
 	void Start ()
@@ -18,15 +19,15 @@ public class PlayerTurret : MonoBehaviour
 		if(usingController)
 		{
 			if(Vector3.Magnitude(new Vector3(Input.GetAxis("RightStickHorizontal"),Input.GetAxis("RightStickVertical"), 0)) > 0.6f)
-            	transform.localEulerAngles = -Mathf.Sign(Input.GetAxis("RightStickVertical")) * Vector3.back * Vector3.Angle(Vector3.right, Vector3.Normalize(new Vector3(Input.GetAxis("RightStickHorizontal"), Input.GetAxis("RightStickVertical"), 0)));
+            	turretChildTransform.transform.localEulerAngles = -Mathf.Sign(Input.GetAxis("RightStickVertical")) * Vector3.back * Vector3.Angle(Vector3.right, Vector3.Normalize(new Vector3(Input.GetAxis("RightStickHorizontal"), Input.GetAxis("RightStickVertical"), 0)));
 		}
 		else
 		{
-			Vector3 v = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10);
+			Vector3 v = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
 			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(v);
  			Quaternion rot = Quaternion.LookRotation (mousePosition - transform.position, transform.TransformDirection(Vector3.forward));
- 			transform.rotation = new Quaternion(0, 0, rot.z, rot.w);
-			transform.Rotate(new Vector3(0,0,90));
+ 			turretChildTransform.transform.rotation = new Quaternion(0, 0, rot.z, rot.w);
+			turretChildTransform.transform.Rotate(new Vector3(0,0,-90));
 		}
 	}
 }
