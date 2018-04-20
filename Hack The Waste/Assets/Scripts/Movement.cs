ff4bc22cdd2176642fea09389ca56f2fd3694c4f
xXsjorsdudeXx <sjors.iven@gmail.com>
2018-04-20 20:17:47 +0200

    Rotation towards cursor, controller also supported. prefab added.

--- Hack The Waste/Assets/Scripts/Movement.cs
+++ Hack The Waste/Assets/Scripts/Movement.cs
@@ -6,18 +6,16 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	private Rigidbody2D rigidbody2D;
-	public float moveSpeed;
+	public float moveSpeed = 5;
 	private Vector2 moveVelocity;
 	private Vector2 moveInput;
 	// Use this for initialization
 	void Start ()
 	{
 		rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D.gravityScale = 0;
 	}
 
-	/// <summary>
-	/// Update is called every frame, if the MonoBehaviour is enabled.
-	/// </summary>
 	void Update()
 	{
 		moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -28,22 +26,5 @@ public class Movement : MonoBehaviour {
 	void FixedUpdate ()
 	{
 		rigidbody2D.velocity = moveVelocity;
-
-		//Controller input
-		// if(Input.GetAxis("LeftStickHorizontal") < -0.19f || Input.GetAxis("LeftStickHorizontal") > 0.19f || Input.GetAxis("LeftStickVertical") < -0.19f || Input.GetAxis("LeftStickVertical") > 0.19f)
-		// {
-		// 	Vector2 currentInput = new Vector2(Input.GetAxis("LeftStickHorizontal") * moveSpeed * Time.deltaTime, Input.GetAxis("LeftStickVertical") * moveSpeed * Time.deltaTime);
-		// 	rigidbody2D.velocity = currentInput;
-		// }
-		//Keyboard input
-
-
-
-		// Input.GetAxis("LeftTrigger");
-		// Input.GetAxis("RightTrigger");
-
- 
-		// Move senteces
-		
 	}
 }
---
+++ Hack The Waste/Assets/Scripts/PlayerTurret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurret : MonoBehaviour
+{
+	private Camera mainCamera;
+	public bool usingController;
+	// Use this for initialization
+	void Start ()
+	{
+		mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(usingController)
+		{
+			if(Vector3.Magnitude(new Vector3(Input.GetAxis("RightStickHorizontal"),Input.GetAxis("RightStickVertical"), 0)) > 0.6f)
+            	transform.localEulerAngles = -Mathf.Sign(Input.GetAxis("RightStickVertical")) * Vector3.back * Vector3.Angle(Vector3.right, Vector3.Normalize(new Vector3(Input.GetAxis("RightStickHorizontal"), Input.GetAxis("RightStickVertical"), 0)));
+		}
+		else
+		{
+			Vector3 v = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10);
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(v);
+ 			Quaternion rot = Quaternion.LookRotation (mousePosition - transform.position, transform.TransformDirection(Vector3.forward));
+ 			transform.rotation = new Quaternion(0, 0, rot.z, rot.w);
+			transform.Rotate(new Vector3(0,0,90));
+		}
+	}
+}
