cb253da7c06bea26f2101612e79de74b8ebd6eb5
xXsjorsdudeXx <sjors.iven@gmail.com>
2018-04-20 21:40:27 +0200

    selecting weapons added, shooting also supported

--- Hack The Waste/Assets/Scripts/PlayerTurret.cs
+++ Hack The Waste/Assets/Scripts/PlayerTurret.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerTurret : MonoBehaviour
 {
+	public Transform projectileParent;
+	public GameObject[] projectilePrefabs;
+	private int selectedWeapon = 0;
 	private Camera mainCamera;
 	public Transform turretChildTransform;
 	public bool usingController;
+
+	public float shootDelayMax = .2f;
+	private float shootDelayTimer = 0f;
 	// Use this for initialization
 	void Start ()
 	{
@@ -16,6 +22,38 @@ public class PlayerTurret : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		
+
+		if((Input.GetButtonDown("LeftBumper") && usingController) || (Input.GetAxis("Mouse ScrollWheel") < 0 && !usingController))
+		{
+			selectedWeapon --;
+			if(selectedWeapon < 0) selectedWeapon = projectilePrefabs.Length -1;
+		}
+
+		if((Input.GetButtonDown("RightBumper") && usingController) || (Input.GetAxis("Mouse ScrollWheel") > 0 && !usingController))
+		{
+			selectedWeapon ++;
+			if(selectedWeapon > projectilePrefabs.Length -1) selectedWeapon = 0;
+		}
+
+		if((Input.GetAxis("RightTrigger") > 0 && usingController) || (Input.GetMouseButton(0) && !usingController))
+		{
+			if(shootDelayTimer > shootDelayMax)
+			{
+				shootDelayTimer = 0f;
+
+				var obj = Instantiate(projectilePrefabs[selectedWeapon]);
+				obj.transform.rotation = turretChildTransform.rotation;
+				obj.transform.position = turretChildTransform.position;
+				if(projectileParent != null) obj.transform.parent = projectileParent;
+			}
+		}
+
+		if(shootDelayTimer < shootDelayMax)
+		{
+			shootDelayTimer += Time.deltaTime;
+		}
+
 		if(usingController)
 		{
 			if(Vector3.Magnitude(new Vector3(Input.GetAxis("RightStickHorizontal"),Input.GetAxis("RightStickVertical"), 0)) > 0.6f)
